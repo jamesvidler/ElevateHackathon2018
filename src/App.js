e119f43aa5f3a65cda977246821fbff1dfb9253c
James Vidler <james.vidler@outlook.com>
2018-09-22 15:16:48 -0400

    merged updates from maste

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { Switch, Route } from 'react-router-dom'
-//import DailyMetric from './DailyMetric'
-import DailyMetricCopy from './DailyMetricCopy'
-import Schedule from './Schedule'
-import { Link } from 'react-router-dom'
+import DailyMetric from './DailyMetric'
 
 var defaultValues = {
   customerID:'1528cf03-ff1e-4647-a76e-390b8b32dcb8_9c8b689c-daec-4fe6-836d-07d36f9dbcc9',
@@ -72,38 +69,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          <h1 className="App-title">Personal Financial Heartbeat</h1>
-          <h3>{this.state.test}</h3>
-        </header> */}
-
-
-        <div className="MainBox">
-          <h4 className="Green">+$1.35</h4>
-          <h2 className="DaySave">$56.13</h2>
-          <h4 className="Red">-$3.25</h4>
-        <h4>SAVED</h4>
-        {/* <div className="groupC">
-          <div className="circle"></div>
-          <div className="activeCircle"></div>
-        </div> */}
-        </div>
-
-
-        {/* <Link to="/">Daily Metric</Link>
-        <Link to="/schedule">Work Schedule</Link> */}
+        
         <main>
-
-        {/* <Switch>
-          <Route exact path='/' render={(props) => <DailyMetric app={this.state} onStateChange={this.updateState} />}/>
->>>>>>> 15cac1115bbfab6446de84c98990b286a19b949f
-          <Route exact path='/schedule' render={(props) => <Schedule app={this.state} />}/>
-        </Switch> */}
+          <Switch>
+            <Route exact path='/' render={(props) => <DailyMetric app={this.state} onStateChange={this.updateState} />}/>
+          </Switch>
         </main>
-
-        {/* <pre> 
-          {JSON.stringify(this.state.workSchedule)}
-        </pre> */}
       </div>
     );
   }
--- src/DailyMetric.js
+++ src/DailyMetric.js
@@ -1,29 +1,117 @@
 import React, { Component } from 'react';
 import './DailyMetric.css';
 import Api from './Api';
+import CountUp from 'react-countup'
 
 class DailyMetric extends Component {
     constructor(props) {
         super(props);
-        this.changeStateTestVal = this.changeStateTestVal.bind(this);
+        
+        //this.changeStateTestVal = this.changeStateTestVal.bind(this);
+        this.increaseBalance = this.increaseBalance.bind(this);
+        this.decreaseBalance = this.decreaseBalance.bind(this);
+        this.state = {
+            oldBalance: 0.00,
+            balance: 0.00,
+            lastIncrease: "$ 0.00",
+            lastDecrease: "$ 0.00",
+            showIncrease: false,
+            showDecrease: false
+        };          
+
+        this.hideTransactionsAfterTimeout = false;
+
+        this.formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+            minimumFractionDigits: 2
+          })
     }
     componentDidMount = function() {
-        //debugger;
-        Api.getCustomerIncome();
+
     }
-    changeStateTestVal = function() {
-        this.props.onStateChange({
-            test: 'a test value',
-            workSchedule: null
+    updateBalance = function(newBalance) {
+        var diff = newBalance - this.state.balance;
+        this.setState({
+            oldBalance: this.state.balance,
+            balance: newBalance,
+            lastIncrease: (diff > 0 ? this.newIncreaseItem(diff) : this.state.lastIncrease),
+            lastDecrease: (diff < 0 ? this.newDecreaseItem(diff) : this.state.lastDecrease)
         })
+    }
+    newIncreaseItem = function(diff) {
+        var self = this;
+        self.setState({
+            showIncrease: true
+        })
+        if(self.hideTransactionsAfterTimeout) {
+            setTimeout(function() {
+                self.setState({
+                    showIncrease: false
+                })
+            }, 3000)
+        }
+        return this.formatter.format(diff);
+    }
+    newDecreaseItem = function(diff) {
+        var self = this;
+        self.setState({
+            showDecrease: true
+        })
+        if(self.hideTransactionsAfterTimeout) {
+            setTimeout(function() {
+                self.setState({
+                    showDecrease: false
+                })
+            }, 3000)
+        }
+        
+        return this.formatter.format(diff);
+    }
+    increaseBalance = function() {
+        var balance = this.state.balance;
+        var newBalance = balance + 300.25;
+        this.updateBalance(newBalance);       
+    }
+    decreaseBalance = function() {
+        var balance = this.state.balance;
+        var newBalance = balance - 5.75;
+        this.updateBalance(newBalance);
     }
     render() {
         return (
-        <div className="DailyMetric">
-            <h1>This is the daily metric component</h1>
-            <p>Customer ID is {this.props.app.customerID}</p>
-            <button onClick={this.changeStateTestVal}>Update Parent Value</button>
-        </div>
+
+            <div className="MainBox">
+                {this.state.showIncrease &&
+                <h4 className="Green">+{this.state.lastIncrease}</h4>
+                }
+                <CountUp
+                    start={this.state.oldBalance}
+                    end={this.state.balance}
+                    duration={2.75}
+                    separator=","
+                    decimals={2}
+                    decimal="."
+                    prefix="$ "
+                    suffix=""
+                    onEnd={() => console.log('Ended! 👏')}
+                    onStart={() => console.log('Started! 💨')}
+                    >
+                    {({ countUpRef }) => (
+                        <h2 className="DaySave" ref={countUpRef}/>
+                    )}
+                </CountUp>
+                {this.state.showDecrease &&
+                    <h4 className="Red">{this.state.lastDecrease}</h4>
+                }
+                <h4>SAVED</h4>
+                {/* <div className="groupC">
+                <div className="circle"></div>
+                <div className="activeCircle"></div>
+                </div> */}
+                <button onClick={this.increaseBalance}>increase balance</button>
+                <button onClick={this.decreaseBalance}>decrease balance</button>
+            </div>
         );
     }
 }
