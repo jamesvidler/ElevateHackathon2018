e39510f5146cd6030df67645a331d2acc9470330
James Vidler <james.vidler@outlook.com>
2018-09-22 11:38:42 -0400

    tested api module

--- src/DailyMetric.js
+++ src/DailyMetric.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import './DailyMetric.css';
+import Api from './Api';
 
 class DailyMetric extends Component {
-  render() {
-    return (
-      <div className="DailyMetric">
-        <h1>This is the daily metric component</h1>
-        <p>Customer ID is {this.props.app.customerID}</p>
-      </div>
-    );
-  }
+    componentDidMount = function() {
+        Api.getCustomerIncome();
+    }
+    render() {
+        return (
+        <div className="DailyMetric">
+            <h1>This is the daily metric component</h1>
+            <p>Customer ID is {this.props.app.customerID}</p>
+        </div>
+        );
+    }
 }
 
 export default DailyMetric;
