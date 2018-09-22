afc025e722b32cc4c312d3749da485077317ee89
James Vidler <james.vidler@outlook.com>
2018-09-22 10:44:24 -0400

    added react router

---
+++ src/DailyMetric.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './DailyMetric.css';
+
+class DailyMetric extends Component {
+  render() {
+    return (
+      <div className="DailyMetric">
+        <h1>This is the daily metric component</h1>
+      </div>
+    );
+  }
+}
+
+export default DailyMetric;
