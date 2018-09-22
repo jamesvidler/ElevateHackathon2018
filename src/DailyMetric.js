47013e605636fcc4333fc77d050c399f3dcf0f8a
James Vidler <james.vidler@outlook.com>
2018-09-22 12:25:14 -0400

    added state update flow

--- src/DailyMetric.js
+++ src/DailyMetric.js
@@ -3,14 +3,25 @@ import './DailyMetric.css';
 import Api from './Api';
 
 class DailyMetric extends Component {
+    constructor(props) {
+        super(props);
+        this.changeStateTestVal = this.changeStateTestVal.bind(this);
+    }
     componentDidMount = function() {
+        //debugger;
         Api.getCustomerIncome();
+    }
+    changeStateTestVal = function() {
+        this.props.onStateChange({
+            test: 'a test value'
+        })
     }
     render() {
         return (
         <div className="DailyMetric">
             <h1>This is the daily metric component</h1>
             <p>Customer ID is {this.props.app.customerID}</p>
+            <button onClick={this.changeStateTestVal}>Update Parent Value</button>
         </div>
         );
     }
